0d6819a08aaf6c245c853cbb8776d21bf92b891b
Tomasz Rybarczyk <paluho@gmail.com>
2013-08-22 20:02:40 +0200

    Add classifires to setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,23 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Pre-Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Code Generators',
+    'Topic :: Software Development :: Libraries :: Python Modules']
+
 setup(
     name='code-formatter',
     author='Tomasz Rybarczyk',
     author_email='paluho@gmail.com',
-    classifiers=[
-        'Programming Language :: Python'
-    ],
+    classifiers=CLASSIFIERS,
     description='',
     dependency_links=[],
     install_requires=[],
